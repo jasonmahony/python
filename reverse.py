f000c9bbae313de29d98436af9a9ff1523e88e9e
Jason Mahony <jason.mahony@gmail.com>
2016-11-22 21:05:58 -0800

    reverse string order

--- reverse.py
+++ reverse.py
@@ -3,6 +3,7 @@
 string = "You this needs to be reversed."
 
 def rorder(string):
-    return " ".join(reversed(string.split()))
+    return " ".join(string.split()[::-1])
+#    return " ".join(reversed(string.split()))
 
 print rorder(string)
