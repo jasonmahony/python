60c4d40f43b5199767528ae215544f113c66e71b
Jason Mahony <jason.mahony@autodesk.com>
2019-01-15 14:37:13 -0800

    updated balanced.py

--- balanced.py
+++ balanced.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
+#https://www.codewars.com/kata/balanced-number-special-numbers-series-number-1/train/python
+
 def balanced_num(number):
-    digits = len(str(number))
-    if int(digits) < 2:
+    lst = list(str(number))
+    digits = len(lst)
+    if int(len(lst)) < 2:
         return "Balanced"
     else:
-        index = list(str(number))
-        if digits % 2 == 0:
-            middle = int(digits) / 2
-            sum_mid = index[middle - 1] + index[middle]
+        if len(lst) % 2 == 0:
+            middle = int(len(lst)) / 2
+            sum_left = sum(list(map(int, lst[0: middle - 1])))
+            sum_right = sum(list(map(int, lst[middle + 1: -1])))
+            return "Balanced" if sum_left == sum_right else "Not Balanced"
+#            sum_left = map(lambda x: sum(int(x)), lst[0: middle - 1])
+#            return sum_left
             #sum_left = map(lambda x: sum(x), index[0: middle - 1])
             #sum_right = sum(index) - sum_left - sum_mid
             #return "Balanced" if sum_right == sum_left else "Not Balanced"
         else:
-            middle = (int(digits) - 1) / 2
-            return index[middle]
-            
-print balanced_num(795)
+            middle = (int(len(lst)) - 1) / 2
+            sum_left = sum(list(map(int, lst[0: middle])))
+            sum_right = sum(list(map(int, lst[middle + 1: -1])))
+            return "Balanced" if sum_left == sum_right else "Not Balanced"
+
+print balanced_num(35335)
+print balanced_num(7935)
+print balanced_num(79358909)
