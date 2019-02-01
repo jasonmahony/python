bb7d7e6ff1f94ad2d614c3a78922c17d1bea7189
Jason Mahony <jason.mahony@gmail.com>
2019-01-31 23:45:03 -0800

    Adding high.py

--- high.py
+++ high.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python
 
-import string
-
 def high(x):
     values = dict()
     for index, letter in enumerate(string.ascii_lowercase):
        values[letter] = index + 1
     words = x.split()
-    word_list = list(enumerate(words))
+    score_card = list()
     for word in words:
         word_score = 0
-        for l in list(word):
-            word_score = word_score + values[l]
-
-    ##    word_score = reduce((lambda l, m: values[l] + values[m]), list(word))
-
+        for c in list(word):
+            word_score = word_score + values[c]
+        score_card.append(word_score)
+    return words[score_card.index(max(score_card))]
 
     
 print high('man i need a taxi up to ubud')
 print high('what time are we climbing up the volcano')
+
+##### Rules
+#Given a string of words, you need to find the highest scoring word.
+#Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
+#You need to return the highest scoring word as a string.
